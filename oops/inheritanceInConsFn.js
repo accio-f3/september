7c492efb6c5e1d7a0633b4b1abd09189dedcc2eb
acciof3@gmail.com <accio-f3>
2023-09-14 22:17:41 +0530

    oops 2-2

--- oops/inheritanceInConsFn.js
+++ oops/inheritanceInConsFn.js
@@ -12,16 +12,23 @@ CreateSavingsAccount.prototype.withdraw = function (amount) {
     this.accountBalance -= amount;
 }
 
-function CreateCurrentAccount(nam, branch, accountBalance) {
+function CreateCurrentAccount(nam, branch, accountBalance, withdrawLimit) {
     // this.nam = nam;
     // this.branch = branch;
     // this.accountBalance = accountBalance;
-    this.withdrawLimit = 500000; // 5 lakhs
+    // THIS keyword inside `CreateCurrentAccount` will be pointing towards
+    // the newly formed object
+    // CreateSavingsAccount(nam, branch, accountBalance); -> here the function will point towards window
+    CreateSavingsAccount.call(this, nam, branch, accountBalance);
+    this.withdrawLimit = withdrawLimit; // 5 lakhs
 }
-
+CreateCurrentAccount.prototype.__proto__ = CreateSavingsAccount.prototype;
 // CreateCurrentAccount.prototype.withdraw = function (amount) {
 //     this.accountBalance -= amount;
 // }
-// CreateCurrentAccount.prototype.businessLoan = function (amount) {
-//     console.log(`you are eligible for this ${amount} of loan`);
-// }
+
+CreateCurrentAccount.prototype.businessLoan = function (amount) {
+    console.log(`you are eligible for this ${amount} of loan`);
+}
+
+const customer1 = new CreateCurrentAccount('pc','icici',200,5000);
